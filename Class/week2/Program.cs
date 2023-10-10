cb55fb8875825051268e32bf3a776b29f4f6685f
Asif Mahmud <r0918559@student.thomasmore.be>
2023-10-10 18:06:33 +0200

    m

---
+++ Class/week2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace week2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 5, 1, 4, 3, 7, 0, 2, 6, 9, 8 };
+
+            Sorting sorting = new Sorting();
+
+            foreach (int i in sorting.Bubble(ints))
+            {
+                Console.WriteLine(i);
+            }
+            foreach (int i in sorting.ReverseBubbleSort(ints))
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
---
+++ Class/week2/Sorting.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week2
+{
+    internal class Sorting
+    {
+        public int[] Bubble(int[] ints)
+        {
+            for (int i = 0; i < ints.Length; i++)
+            {
+                for (int j = 0; j < ints.Length - 1; j++)
+                {
+                    int temp = ints[j];
+                    int value1 = ints[j];
+                    int value2 = ints[j + 1];
+
+                    if (ints[j] > ints[j + 1])
+                    {
+                        ints[j] = ints[j + 1];
+                        ints[j + 1] = temp;
+                    }
+                }
+            }
+            return ints;
+        }
+        public int[] ReverseBubbleSort(int[] ints)
+        {
+            for (int i = 0; i < ints.Length; i++)
+            {
+                for (int j = 0; j < ints.Length - 1; j++)
+                {
+                    int temp = ints[j];
+                    int value1 = ints[j];
+                    int value2 = ints[j + 1];
+
+                    if (ints[j] < ints[j + 1])
+                    {
+                        ints[j] = ints[j + 1];
+                        ints[j + 1] = temp;
+                    }
+                }
+            }
+
+            return ints;
+        }
+        public override string ToString()
+        {
+
+        }
+    }
+}
