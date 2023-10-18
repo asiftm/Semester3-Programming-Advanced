9be8e02d51d7a1c5ac293247980e020ca962174a
Asif Mahmud <r0918559@student.thomasmore.be>
2023-10-18 18:20:41 +0200

    m

---
+++ PracticeRecursion/Practice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeRecursion
+{
+    internal class Practice
+    {
+        public int Factorial(int a)
+        {
+            int result = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+        public int FactorialRecursion(int a)
+        {
+            if (a < 1) return 1;
+            return a * FactorialRecursion(a-1);
+        }
+        public int Sum(int[] array)
+        {
+            int result = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                result += array[i];
+            }
+            return result;
+        }
+        public int SumRecursion(int[] array)
+        {
+            return 8;
+        }
+    }
+}
---
+++ PracticeRecursion/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PracticeRecursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Practice practice = new Practice();
+
+            Console.WriteLine(practice.Factorial(6));
+            Console.WriteLine(practice.FactorialRecursion(6));
+
+            int[] array = {2, 6, 9, 8, 4, 1, 7, 3, 5};
+            Console.WriteLine(practice.Sum(array));
+            Console.WriteLine(practice.SumRecursion(array));
+        }
+    }
+}
