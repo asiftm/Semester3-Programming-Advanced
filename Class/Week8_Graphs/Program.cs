048af93a5474a9946583a3bebd281ba2936921fa
Asif Mahmud <r0918559@student.thomasmore.be>
2023-11-23 23:20:05 +0100

    g

--- Class/Week8_Graphs/Program.cs
+++ Class/Week8_Graphs/Program.cs
@@ -1,43 +1,38 @@
-﻿using System;
-using System.ComponentModel;
-
-namespace Week8_Graphs
+﻿namespace Week8_Graphs
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Maze_Class maze_class = new Maze_Class(25);
+            Maze maze = new Maze(25);
 
-            maze_class.AddEdge(0, 1);
-            maze_class.AddEdge(1, 2);
-            maze_class.AddEdge(2, 3);
-            maze_class.AddEdge(3, 4);
-            maze_class.AddEdge(4, 9);
-            maze_class.AddEdge(9, 14);
-            maze_class.AddEdge(18, 17);
-            maze_class.AddEdge(18, 19);
-            maze_class.AddEdge(19, 14);
-            maze_class.AddEdge(19, 24);
-            maze_class.AddEdge(24, 23);
-            maze_class.AddEdge(18, 23);
-            maze_class.AddEdge(22, 23);
-            maze_class.AddEdge(17, 12);
-            maze_class.AddEdge(21, 22);
-            maze_class.AddEdge(21, 20);
-            maze_class.AddEdge(20, 15);
-            maze_class.AddEdge(15, 10);
-            maze_class.AddEdge(10, 5);
-            maze_class.AddEdge(5, 0);
+            maze.AddEdge(0, 1);
+            maze.AddEdge(1, 2);
+            maze.AddEdge(2, 3);
+            maze.AddEdge(3, 4);
+            maze.AddEdge(4, 9);
+            maze.AddEdge(9, 14);
+            maze.AddEdge(18, 17);
+            maze.AddEdge(18, 19);
+            maze.AddEdge(19, 14);
+            maze.AddEdge(19, 24);
+            maze.AddEdge(24, 23);
+            maze.AddEdge(18, 23);
+            maze.AddEdge(22, 23);
+            maze.AddEdge(17, 12);
+            maze.AddEdge(21, 22);
+            maze.AddEdge(21, 20);
+            maze.AddEdge(20, 15);
+            maze.AddEdge(15, 10);
+            maze.AddEdge(10, 5);
+            maze.AddEdge(5, 0);
 
-            Console.WriteLine(maze_class.ToString());
+            Console.WriteLine(maze.ToString());
 
-            List<int> solve = maze_class.DFS_Recursion(12, new List<int> { } , 10);
-            Console.WriteLine("DFS_Recursion: " + string.Join("  ",solve));
-
-            Console.WriteLine("DFS_Stack:     " + maze_class.DFS_Stack(12, 10).Trim());
-
-            Console.WriteLine("BFS_Queue:     " + maze_class.BFS_Queue(12, 10).Trim());
+            List<int> nodes = maze.DFS(12, null);
+            Console.WriteLine("DFS RECUSION:" + String.Join("--> ", nodes));
+            Console.WriteLine("DFS STACK: " + maze.DFS(12,0));
+            Console.WriteLine("BFS:" + maze.BFS(12, 0));
 
 
 
---
+++ Class/week9/WeightedGraphs/Maze.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week9_WeightedGraphs
+{
+    class Maze
+    {
+        int nodes;
+        List<int>[] graph;
+        public Maze(int nodes)
+        {
+            this.nodes = nodes;
+            graph = new List<int>[nodes]; //create graph with nr of nodes
+            for (int i = 0; i < nodes; i++) //init all lists
+            {
+                graph[i] = new List<int>();
+            }
+        }
+        public void AddEdge(int node1, int node2)
+        {
+            //to make it better, check if a node already is in the list. not going out of bounds
+            graph[node1].Add(node2);
+            graph[node2].Add(node1);
+        }
+        public void Dijkstra(int start, int end)
+        {
+            int[] distances = new int[nodes];
+
+            for (int i = 0;i < nodes;i++)
+            {
+                distances[i] = Int32.MaxValue;
+            }
+
+            int[] previous = new int[nodes];
+
+            List<int> visited = new List<int>();
+
+            distances[start] = 0;
+
+            while (true)
+            {
+                int next = GetNextNode(distances, visited);
+
+                if (next == -1 || next == end) break;
+
+                visited.Add(next);
+
+                foreach (int neighbour in graph[next])
+                {
+                    int cost = distances[next] + Math.Abs(neighbour - next);
+
+                    if (cost < distances[neighbour])
+                    {
+                        distances[neighbour] = cost;
+                        previous[neighbour] = next;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join("  ",distances));
+
+            string path = "";
+            int node = 0;
+
+            while(node != start)
+            {
+                break;
+            }
+
+            for (int i = 0; i < nodes; i++)
+            {
+                Console.WriteLine("cost to node {i} --> {distences[i]}");
+            }
+        }
+        private int GetNextNode(int[] distances, List<int> visited)
+        {
+            int next = -1;
+            int min = Int32.MaxValue;
+
+            for (int i = 0; i < nodes; i++)
+            {
+                if (distances[i] < min && !visited.Contains(i))
+                {
+                    min = distances[i];
+                    next = i;
+                }
+            }
+            return next;
+        }
+        public override string ToString()
+        {
+            string s = "";
+            for (int i = 0; i < nodes; i++)
+            {
+                s += i + " --> " + String.Join(" ", graph[i]) + "\n";
+            }
+            return s;
+        }
+    }
+}
