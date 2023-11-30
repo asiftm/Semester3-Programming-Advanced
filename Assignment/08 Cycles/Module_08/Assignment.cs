b2d5722ff104fa6c04f4d712333cf91a2c50627c
Asif Mahmud <r0918559@student.thomasmore.be>
2023-11-30 20:42:37 +0100

    8-1

---
+++ Assignment/08 Cycles/Module_08/Assignment.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_08
+{
+    class Assignment
+    {
+        int Nodes;
+        List<int>[] Graph;
+        public Assignment(int nodes)
+        {
+            this.Nodes = nodes;
+            Graph = new List<int>[Nodes];
+            for (int i = 0; i < nodes; i++)
+            {
+                Graph[i]= new List<int>();
+            }
+        }
+        public void AddEdge(int node1, int node2)
+        {
+            Graph[node1].Add(node2);
+        }
+        public string DFS()
+        {
+            for (int i = 0; i < Graph.Length; i++)
+            {
+                if (Graph[i].Contains(i)) return "cyclic";
+                if (Graph[i].Count > 0)
+                {
+                    List<int> visited = new List<int>();
+                    Stack<int> stack = new Stack<int>();
+
+                    stack.Push(Graph[i][0]);
+
+                    while (stack.Count > 0)
+                    {
+                        int node = stack.Pop();
+
+                        foreach (int next in Graph[node])
+                        {
+                            if (!visited.Contains(next)) visited.Add(next);
+                            else return "cyclic";
+                            stack.Push(next);
+                        }
+                    }
+                }
+            }
+            return "acyclic";
+        }
+    }
+}
---
+++ Assignment/08 Cycles/Module_08/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Module_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int numberOfNodes = Convert.ToInt32(Console.ReadLine());
+                int numberOfEdges = Convert.ToInt32(Console.ReadLine());
+
+                Assignment assignment = new Assignment(numberOfNodes);
+
+                for (int i = 0; i < numberOfEdges; i++)
+                {
+                    string[] pair = Console.ReadLine().Split(" ");
+                    assignment.AddEdge(int.Parse(pair[0]), int.Parse(pair[1]));
+                }
+
+                Console.WriteLine(assignment.DFS());
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+            
+
+        }
+    }
+}
