14c6271516f026dd5e3ddb844f1199c091ab4a96
Asif Mahmud <r0918559@student.thomasmore.be>
2023-12-21 00:42:00 +0100

    a

---
+++ Assignment/11 Similar/Module_11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Module_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string input1 = Console.ReadLine().Trim();
+                string input2 = Console.ReadLine().Trim();
+
+                Similar similar = new Similar(input1);
+                Console.WriteLine(similar.IsSimilar(input1, input2));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Crazy input!");
+            }
+        }
+    }
+}
---
+++ Assignment/11 Similar/Module_11/Similar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Module_11
+{
+    internal class Similar
+    {
+        Dictionary<char,char> Chart;
+
+        public Similar(string input)
+        {
+            Chart = new Dictionary<char, char>(input.Length);
+        }
+        public string IsSimilar(string input1, string input2)
+        {
+            for (int i = 0; i < input1.Length; i++)
+            {
+                if (!Chart.ContainsKey(input2[i]))
+                {
+                    Chart[input2[i]] = input1[i];
+                }
+                else if (Chart[input2[i]] != input1[i])
+                {
+                    return "False";
+                }
+            }
+            return "True";
+        }
+    }
+}
