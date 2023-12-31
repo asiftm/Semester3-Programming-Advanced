55d62da2af4693ce84816a546a4fff27e97bc2d0
Asif Mahmud <r0918559@student.thomasmore.be>
2023-12-31 10:43:24 +0100

    ll

--- Assignment/05 Magic Square/Module_05/MagicSquare.cs
+++ Assignment/05 Magic Square/Module_05/MagicSquare.cs
@@ -10,15 +10,16 @@ namespace Module_05
     public class MagicSquare
     {
         public int[,] Square { get; set; } //must be public property so I can play with in the Unit-Test
-        private int size;
-
+        // Square is a 2D dictionary to show the blank spaces of a magic square
+        private int size; //Square
+        
         public MagicSquare(int square_size) // constructor
         {
-            Square = new int[square_size,square_size];
+            Square = new int[square_size,square_size]; // initialize the 2D array with n numbers of row and columns
 
             for (int i = 0; i < square_size; i++)
             {
-                for (int j = 0; j < square_size; j++)
+                for (int j = 0; j < square_size; j++) //fill the array with zeros. later it will be replaced with 1 to n^2
                 {
                     Square[i,j] = 0;
                 }
@@ -27,7 +28,7 @@ namespace Module_05
         }
         public string Create(int row = 0, int col = 0, int placed = 0) //create magic square
         {
-            if(row == size && col == 0)
+            if(row == size && col == 0) //
             {
                 Console.WriteLine(ToString());
                 if (IsMagic()) return "solution :\n" + ToString();
@@ -66,7 +67,8 @@ namespace Module_05
             {
                 for (int j = 0; j < size; j++)
                 {
-                    if (Square[i, j] == number) return false;
+                    if (Square[i, j] == number) return false; // checking every index of the array if it alreday contains the number
+                    //if so then 
                 }
             }
             return true;
@@ -75,7 +77,7 @@ namespace Module_05
         {
             int magicConstant = (int)(size * ((Math.Pow(size, 2) + 1)) / 2);
             int sum = 0;
-
+            
             //check rows
             for (int i = 0; i < size; i++)
             {
