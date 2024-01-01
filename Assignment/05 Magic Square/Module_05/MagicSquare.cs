cb286e6646301f497d3b3525ca97fd1ef6caddbd
Asif Mahmud <r0918559@student.thomasmore.be>
2024-01-02 00:06:00 +0100

    m

--- Assignment/05 Magic Square/Module_05/MagicSquare.cs
+++ Assignment/05 Magic Square/Module_05/MagicSquare.cs
@@ -11,7 +11,7 @@ namespace Module_05
     {
         public int[,] Square { get; set; } //must be public property so I can play with in the Unit-Test
         // Square is a 2D dictionary to show the blank spaces of a magic square
-        private int size; //Square
+        private int size; //Square size
         
         public MagicSquare(int square_size) // constructor
         {
@@ -26,29 +26,44 @@ namespace Module_05
             }
             size = square_size;
         }
-        public string Create(int row = 0, int col = 0, int placed = 0) //create magic square
+        public string Create(int row = 0, int col = 0, int placed = 0) //create magic square. we have to start filling the indexes from 00 index in the 2D matrix, that is why the default value for row and column is 0.
         {
-            if(row == size && col == 0) //
+            /*
+             row and column starts at index 0. so if the size of the matrix is 3 then rows will be 0,1,2
+            in this condition below i just checking if the number of rows become higher then the size.
+            if it does then stop inserting numbers
+             */
+
+            if(row == size && col == 0) 
             {
                 Console.WriteLine(ToString());
                 if (IsMagic()) return "solution :\n" + ToString();
                 return "";
             }
+
+            // go to every index in the 2D array and see if int i is valid or not in that index
+            
             for (int i = 1; i < (size*size)+1; i++)
             {
                 if (IsValid(i))
                 {
-                    Square[row, col] = i;
-                    Tuple<int, int> tuple = NextPosition(row, col);
-                    string output = Create(tuple.Item1, tuple.Item2, placed + 1);
-                    if (output.StartsWith("solution")) return output;
-                    Square[row, col] = 0;
+                    // if IsValid == True then -->
+
+                    Square[row, col] = i; // fill the index with that number
+
+                    Tuple<int, int> tuple = NextPosition(row, col);// find the nxt position to do the same
+                    
+                    string output = Create(tuple.Item1, tuple.Item2, placed + 1); // creating a variable to store the output string
+
+                    if (output.StartsWith("solution")) return output; // if you find the solution then stop the loop
+                    Square[row, col] = 0; // if it fail to find the solution then rewrite the index with 0
                 }
             }
             return "";
         }
         public Tuple<int, int> NextPosition(int row, int col) //find next position to fill (next column, or next column and first column again)
         {
+            // conditions to find the next row and column
             if (row > size-1) return Tuple.Create(size, size);
             if (col == size-1) return Tuple.Create(row + 1, 0);
             
@@ -68,7 +83,6 @@ namespace Module_05
                 for (int j = 0; j < size; j++)
                 {
                     if (Square[i, j] == number) return false; // checking every index of the array if it alreday contains the number
-                    //if so then 
                 }
             }
             return true;
--- Assignment/05 Magic Square/Module_05/Program.cs
+++ Assignment/05 Magic Square/Module_05/Program.cs
@@ -7,10 +7,20 @@ namespace Module_05
         {
             try
             {
+                /*
+                 just followed the instruction given in the assignment. 
+                 */
+
+                // first take an input. this is the size of our matrix
                 int input = Convert.ToInt32(Console.ReadLine());
 
+                // making out matrix at the time of declearing the MagicSquare type variable. it can be done later too
                 MagicSquare magicSquare = new MagicSquare(input);
+
+                // creating out solution
                 string output = magicSquare.Create();
+
+                
                 Console.WriteLine(output.Replace("solution :\n", ""));
             }
             catch
